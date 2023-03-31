46b8ed5d2079d3d153554aa9b54e0f42ffdfd9f2
destroyer384 <128086803+destroyer384@users.noreply.github.com>
2023-03-31 18:43:58 +0300

    Create Answer in clipboard.lua
    A script for answer or die roblox game, maybe supported only on synapse x

---
+++ Answer in clipboard.lua
@@ -0,0 +1,60 @@
+-- Get the required instances
+local Players = game:GetService("Players")
+local player = Players["super_destroyer384"]
+local questionTxt = player.PlayerGui.Main.Question.Bg.QuestionTxt
+
+-- Define a dictionary with questions and answers
+local questionAnswerDict = {
+  ["Name a popular vegetable"] = "Sweet potato",
+  ["Name something you eat with"] = "Chopsticks",
+  ["Name a popular Superhero"] = "Captain america",
+  ["Name any month that has 31 days"] = "December",
+  ["Name one of the world's hottest countries"] = "Burkina Faso",
+  ["Name a food that starts with the letter P"] = "Passionfruit",
+  ["Name a day of the week"] = "Wednesday",
+  ["Name something people wear"] = "Necklace",
+  ["Name a planet that is part of our solar system"] = "Neptune",
+  ["Name a type of weather"] = "Stormy",
+  ["Name something you do in your sleep"] = "Nightmare",
+  ["Name one of the four seasons"] = "Winter",
+  ["Name something you do at school"] = "Physical education",
+  ["Name any natural hair color"] = "Brunette",
+  ["Name any capital city in Europe"] = "Luxembourg",
+  ["Name a country that starts with the letter A"] = "Afghanistan",
+  ["Name one of the world's coldest countries"] = "Antarctica",
+  ["Name one of the seven colors of the rainbow"] = "Purple",
+  ["Name a popular fruit"] = "Passionfruit",
+  ["Name a famous Roblox Youtuber"] = "Inquisitormaster",
+  ["Name one of the world's most popular car colors"] = "Silver",
+  ["Name an animal that walks slowly"] = "Tortoise",
+  ["Name any part of the head"] = "Forehead",
+  ["Name a popular electronic device"] = "Electricguitar",
+  ["Name something you find on pizza"] = "Pepperoni",
+  ["Name one of Santa's nine reindeers that show up in Christmas"] = "Rudolph",
+  ["Name a topping that is usually in a hamburger"] = "Mayonnaise",
+  ["Name a musical instrument"] = "Electricguitar",
+  ["Name any popular animal that starts with the letter C"] = "Chameleon",
+  ["Name a popular game on Roblox"] = "Murder Mystery 2",
+  ["Name a social media app"] = "Instagram",
+  ["Name a famous sport that is played in teams"] = "Basketball",
+  ["Name one of the fastest animals"] = "Cheetah",
+  ["Name an animal that can fly"] = "Hummingbird",
+  ["What is something you eat with your hands"] = "Chicken nuggets",
+  ["What is something you can sit on"] = "Gaming chair",
+}
+
+
+-- Define a function to handle property changes
+local function onTextChanged()
+    local question = questionTxt.Text
+
+    local answer = questionAnswerDict[question]
+    if answer then
+        setclipboard(answer)
+    else
+        print(question)
+    end
+end
+
+-- Connect the function to the Text property change signal
+questionTxt:GetPropertyChangedSignal("Text"):Connect(onTextChanged)
