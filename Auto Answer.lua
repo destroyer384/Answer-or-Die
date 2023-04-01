b9a4a03ab07c26861f418cc117605fca617265c4
destroyer384 <128086803+destroyer384@users.noreply.github.com>
2023-04-01 21:35:15 +0300

    Update Auto Answer.lua

--- Auto Answer.lua
+++ Auto Answer.lua
@@ -50,7 +50,7 @@ local function onTextChanged()
 
     local answer = questionAnswerDict[question]
     if answer then
-        wait(8.5)
+        wait(8)
         local args = {
             [1] = "S_System_SubmitAnswer",
             [2] = {
