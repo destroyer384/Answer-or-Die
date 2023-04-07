f80acde4ba1d49201781263583df789170bbfaa0
destroyer384 <128086803+destroyer384@users.noreply.github.com>
2023-04-07 13:38:04 +0300

    Add realistic typing speed

--- Auto Answer.lua
+++ Auto Answer.lua
@@ -49,7 +49,7 @@ local function onTextChanged()
     local question = questionTxt.Text
     local answer = getAnswer[question]
     if answer then
-        wait(9)
+        wait( 5 + (string.len(answer) / 3) )
         local args = {
             [1] = "S_System_SubmitAnswer",
             [2] = {
