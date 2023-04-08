669658d549f8edea309f0e65990c9e547fc6bf71
destroyer384 <128086803+destroyer384@users.noreply.github.com>
2023-04-08 21:03:30 +0300

    Patches +
    Better stability, better performance, better readability.

--- Auto Answer.lua
+++ Auto Answer.lua
@@ -9,137 +9,127 @@ local Towers = Workspace["__MAP"].Rooms -- 8 towers
 
 -- Define a dictionary with questions and answers
 local getAnswer = {
-    ["Name a popular vegetable"] = "Sweetpotato",
-    ["Name something you eat with"] = "Icecreamspoon",
-    ["Name a popular Superhero"] = "Captainamerica",
-    ["Name any month that has 31 days"] = "December",
-    ["Name one of the world's hottest countries"] = "UnitedArabEmirates",
-    ["Name a food that starts with the letter P"] = "Passionfruit",
-    ["Name a day of the week"] = "Wednesday",
-    ["Name something people wear"] = "Contactlense",
-    ["Name a planet that is part of our solar system"] = "Neptune",
-    ["Name a type of weather"] = "Thunderstorm",
-    ["Name something you do in your sleep"] = "Nightmare",
-    ["Name one of the four seasons"] = "Winter",
-    ["Name something you do at school"] = "Physicaleducation",
-    ["Name any natural hair color"] = "Brunette",
-    ["Name any capital city in Europe"] = "Andorralavella",
-    ["Name a country that starts with the letter A"] = "Antiguaandbarabuda",
-    ["Name one of the world's coldest countries"] = "United StatesofAmerica",
-    ["Name one of the seven colors of the rainbow"] = "Purple",
-    ["Name a popular fruit"] = "Passionfruit",
-    ["Name a famous Roblox Youtuber"] = "Inquisitormaster",
-    ["Name one of the world's most popular car colors"] = "Silver",
-    ["Name an animal that walks slowly"] = "Caterpillar",
-    ["Name any part of your head"] = "Forehead",
-    ["Name a popular electronic device"] = "Electricguitar",
-    ["Name something you find on pizza"] = "Americancheese",
-    ["Name one of Santa's nine reindeers that show up in Christmas"] = "Rudolph",
-    ["Name a topping that is usually in a hamburger"] = "Americancheese",
-    ["Name a musical instrument"] = "Electricguitar",
-    ["Name any popular animal that starts with the letter C"] = "Caterpillar",
-    ["Name a popular game on Roblox"] = "NaturalDisasterSurvival",
-    ["Name a social media app"] = "Facebookmessenger",
-    ["Name a famous sport that is played in teams"] = "Americanfootball",
-    ["Name one of the fastest animals"] = "Swordfish",
-    ["Name an animal that can fly"] = "Westernhoneybee",
-    ["What is something you eat with your hands"] = "Chickennuggets",
-    ["What is something you can sit on"] = "Rockingchair",
-    ["undefined"] = "lol",
+    ["Name a popular vegetable"] = "sweetpotato",
+    ["Name something you eat with"] = "icecreamspoon",
+    ["Name a popular Superhero"] = "captainamerica",
+    ["Name any month that has 31 days"] = "december",
+    ["Name one of the world's hottest countries"] = "unitedarabemirates",
+    ["Name a food that starts with the letter P"] = "passionfruit",
+    ["Name a day of the week"] = "wednesday",
+    ["Name something people wear"] = "contactlense",
+    ["Name a planet that is part of our solar system"] = "neptune",
+    ["Name a type of weather"] = "thunderstorm",
+    ["Name something you do in your sleep"] = "nightmare",
+    ["Name one of the four seasons"] = "winter",
+    ["Name something you do at school"] = "physicaleducation",
+    ["Name any natural hair color"] = "brunette",
+    ["Name any capital city in Europe"] = "andorralavella",
+    ["Name a country that starts with the letter A"] = "antiguaandbarabuda",
+    ["Name one of the world's coldest countries"] = "unitedStatesofAmerica",
+    ["Name one of the seven colors of the rainbow"] = "purple",
+    ["Name a popular fruit"] = "passionfruit",
+    ["Name a famous Roblox Youtuber"] = "inquisitormaster",
+    ["Name one of the world's most popular car colors"] = "silver",
+    ["Name an animal that walks slowly"] = "caterpillar",
+    ["Name any part of your head"] = "forehead",
+    ["Name a popular electronic device"] = "electricguitar",
+    ["Name something you find on pizza"] = "mozzarellacheese",
+    ["Name one of Santa's nine reindeers that show up in Christmas"] = "rudolph",
+    ["Name a topping that is usually in a hamburger"] = "americancheese",
+    ["Name a musical instrument"] = "electricguitar",
+    ["Name any popular animal that starts with the letter C"] = "caterpillar",
+    ["Name a popular game on Roblox"] = "naturaldisastersurvival",
+    ["Name a social media app"] = "facebookmessenger",
+    ["Name a famous sport that is played in teams"] = "americanfootball",
+    ["Name one of the fastest animals"] = "swordfish",
+    ["Name an animal that can fly"] = "westernhoneybee",
+    ["What is something you eat with your hands"] = "chickennuggets",
+    ["What is something you can sit on"] = "rockingchair",
+    ["undefined"] = string.rep("L", 100),
 }
 
-local MyBlocks = 0
-local MaxLetters = 0
-local MaxLettersIndex = 1
+local MyAnswerLength = 2
+local MaxAnswerLength = 1
+local MaxAnswerLengthIndex = 1
 local EveryAnswer = {}
-local Blocks = 0
 local LastQuestion = "undefined"
+local CurrentQuestion = "undefined"
 
 local function resetData()
-    if Gui.Question.Bg.QuestionTxt.Text then
-        MyBlocks = string.len(getAnswer[Gui.Question.Bg.QuestionTxt.Text])
-        MaxLetters = 0
-        MaxLettersIndex = 1
-        EveryAnswer = {}
-        Blocks = 0
-    end
+    CurrentQuestion = Gui.Question.Bg.QuestionTxt.Text
+    MyAnswerLength = string.len(getAnswer[CurrentQuestion])
+    MaxAnswerLength = 1
+    MaxAnswerLengthIndex = 1
+    EveryAnswer = {}
 end
+
 
 local function getOthersAnswers()
     local answers = {}
     -- Go through all 8 towers
     for i = 1, 8 do
-        -- Clear text every loop
-        local text = ""
+        -- Clear every loop
+        local letters = ""
         -- Collect text from each block
         for _, v in ipairs(Towers[tostring(i)].Letters:GetChildren()) do
-            text = v.SurfaceGui.Main.LetterTxt.Text .. text
+            letters = v.SurfaceGui.Main.LetterTxt.Text .. letters
         end
-        -- Assign text to index
-        answers[i] = text
+        -- Assign letters to a tower
+        answers[i] = letters
     end
     -- Return dictionary
     return answers
 end
 
+
 local function countDashes(String)
-    local Count = 0
-    for i = 1, #String do
-        if String:sub(i,i) == "-" then
-            Count = Count + 1
-        end
-    end
+    local _, Count = String:gsub("-", "")
     return Count
 end
 
-local function toInt(String)
-    return tonumber(String)
-end
 
-local function onBlocksAdded(newBlock)
+local function updateMaxLetters(newBlock)
     -- Accept only gains
     if newBlock.Name == "BlockGainTemplate" then
         -- Get name and amount of gained blocks
         local announcementText = newBlock.BlockGainTxt
-        local Player, LastAnswerBlocks = announcementText.Text:match("(.+) submitted their answer and gained (%d+) blocks.")
-        local success, _ = pcall(toInt, LastAnswerBlocks)
-
-        if success then
-            LastAnswerBlocks = tonumber(LastAnswerBlocks) -- Make Blocks match its purpose
-
+        local _, LastAnswerBlocks = announcementText.Text:match("(.+) submitted their answer and gained (%d+) blocks.")
+        
+        -- Dont raise an error if 2x blocks encountered
+        pcall(function() 
+            LastAnswerBlocks = tonumber(LastAnswerBlocks)
             -- If new answer is longer than the previous one and mine, then update the longest answer
-            if LastAnswerBlocks > MaxLetters and LastAnswerBlocks > MyBlocks then
-                MaxLetters = LastAnswerBlocks
-                if Gui.Question.Bg.QuestionTxt.Text then
-                    print("Most blocks:", MaxLetters, "Your blocks:", MyBlocks, '(' .. getAnswer[Gui.Question.Bg.QuestionTxt.Text] .. ')')
-                end
+            if LastAnswerBlocks > MaxAnswerLength and LastAnswerBlocks > MyAnswerLength then
+                MaxAnswerLength = LastAnswerBlocks
+                print("Most blocks:", MaxAnswerLength, "Your blocks:", MyAnswerLength, '(' .. getAnswer[CurrentQuestion] .. ')')
             end
-        else
-            print("2x blocks detected.")
-        end
+        end)
     end
 end
 
+
 -- Get the index of a string with the longest answer
 local function parseLongest()
-    -- Get the letters
     EveryAnswer = getOthersAnswers()
     -- Loop and check for longest answer's index
     for i = 1, #EveryAnswer do
-        -- When longest found
-        if countDashes(EveryAnswer[i]) == MaxLetters then
+        if countDashes(EveryAnswer[i]) == MaxAnswerLength then
             -- Get the index
-            MaxLettersIndex = i
+            MaxAnswerLengthIndex = i
             break
          end
     end
 end
 
 
-local function getTheAnswer()
+local function getABetterAnswer()
+    local LongestAnswer = string.sub(getOthersAnswers()[MaxAnswerLengthIndex], 1, MaxAnswerLength)
+    wait(string.len(LongestAnswer / 2))
     -- Print raw answer
     print("     Question:", LastQuestion)
-    print("     Got the answer:", string.sub(getOthersAnswers()[MaxLettersIndex], 1, MaxLetters))
+    print("     Got the answer:", LongestAnswer)
+    getAnswer[LastQuestion] = string.sub(EveryAnswer[MaxAnswerLengthIndex], 1, MaxAnswerLength)
+    print("")
 end
 
 
@@ -147,22 +137,23 @@ end
 local function onTimerUpdate()
     if Gui.Question.Bg.TimerTxt.Text == "00:01" then
         parseLongest()
-        LastQuestion = Gui.Question.Bg.QuestionTxt.Text
+        LastQuestion = CurrentQuestion
     end
 end
+
 
 -- If somebody beat me in the previous round, get the answer he entered
 -- Reset variables from the prev. round and submit my answer
 local function onQuestionUpdate()
-    if MyBlocks < MaxLetters then
-        getTheAnswer()
+    if MyAnswerLength < MaxAnswerLength then
+        getABetterAnswer()
     end
     wait(0.25)
 
     -- Reset data from previous question
     pcall(resetData)
 
-    local Answer = getAnswer[Gui.Question.Bg.QuestionTxt.Text]
+    local Answer = getAnswer[CurrentQuestion]
     if Answer then
         wait( 6 + (string.len(Answer) / 4) )
         game:GetService("ReplicatedStorage").Common.Library.Network.RemoteFunction:InvokeServer("S_System_SubmitAnswer", {Answer})
@@ -173,4 +164,4 @@ onQuestionUpdate()
 
 Gui.Question.Bg.QuestionTxt:GetPropertyChangedSignal("Text"):Connect(onQuestionUpdate)
 Gui.Question.Bg.TimerTxt:GetPropertyChangedSignal("Text"):Connect(onTimerUpdate)
-Gui.BlockGain.ChildAdded:Connect(onBlocksAdded)
+Gui.BlockGain.ChildAdded:Connect(updateMaxLetters)
